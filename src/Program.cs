ed0807c8ab7139fb702ae211fedbf5edbef06c0c
Radu Tomuleasa <radutomy@gmail.com>
2020-05-08 14:07:31 +0100

    added nuget packages

--- src/Program.cs
+++ src/Program.cs
@@ -12,7 +12,6 @@ namespace RealTimeBlazor
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
-
 			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			await builder.Build().RunAsync();
